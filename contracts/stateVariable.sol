2259a2398335062aef03c8d212b848209f4fa70f
leomessi-101 <leo0707200@gmail.com>
2025-07-22 15:32:49 +0530

    initial commit

---
+++ contracts/stateVariable.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+
+contract stateVariable {
+    uint public number;
+
+    constructor() {
+        number = 10;
+    }
+
+    function setter() public {
+        number=100;
+    }
+}
